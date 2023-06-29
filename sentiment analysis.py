c6a3e0f431d2c18031952dd085b010c298128dc4
Eiznel <122499682+Eiznel@users.noreply.github.com>
2023-06-29 21:19:58 +0200

    Add files via upload

---
+++ sentiment analysis.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+plt.style.use('ggplot')
+
+import nltk
+
+df = pd.read_csv('data_with_IVscores.csv')
